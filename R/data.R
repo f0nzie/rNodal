1d3b366d7a3e9693a1222e8a751792636ea71903
f0nzie zbook <zbook@oilgainsanalytics.com.com>
2017-06-01 13:30:17 -0500

    document Fancher and Brown digitized dataset

--- R/data.R
+++ R/data.R
@@ -4,3 +4,11 @@
 #' @keywords datasets
 #' @docType data
 "dpt"
+
+
+#' Digitized version of Fancher and Brown curve. Brown Chapter 2 figure 41
+#'
+#' @usage data(curves.0241)
+#' @keywords datasets
+#' @docType data
+"curves.0241"
