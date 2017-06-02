d267e4f742da9fb7a82f2df4bd8ad1517040ac59
f0nzie zbook <zbook@oilgainsanalytics.com.com>
2017-06-02 09:51:18 -0500

    comment code creating data folder onLoad

--- R/zzz.R
+++ R/zzz.R
@@ -6,6 +6,14 @@
     # print("loading ...")
 
     setProjectEnvironment()
+
+    # # code to copy a h5 file to a user folder
+    # theFolder <- "extdata"
+    # .source <- system.file(package = "rNodal", "extdata", "default.hdf5")
+    # .target <- paste(theFolder, "default.hdf5", sep = "/")
+    #
+    # if (!dir.exists(theFolder)) dir.create(theFolder)
+    # if (!file.exists(.target)) file.copy(from = .source, to = .target)
 }
 
 
