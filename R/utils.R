cd085b3d1705c8f244fe0b50423c3811f4b8ec54
f0nzie zbook <zbook@oilgainsanalytics.com.com>
2017-06-03 15:59:55 -0500

    add new functions to write to hdf5 files

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,14 @@
 
 
+
+useLatestHdf5 <- function() {
+    if (isHdf5Files())
+        hdf5_files <- listAllHdf5()
+    else
+        stop("No HDF5 files found")
+
+}
+
 #' Logical response to presence of HDF5 files anywhere under user root folder
 #' @export
 isHdf5Files <- function() {
@@ -10,12 +19,63 @@ isHdf5Files <- function() {
 #'
 #' @export
 listAllHdf5 <- function() {
-    user_root_folder <- rprojroot::find_rstudio_root_file()
-    list.files(path = user_root_folder, pattern = "*.h5$|*.hdf5$",
-               all.files = FALSE, full.names = FALSE, recursive = TRUE,
-               ignore.case = FALSE, include.dirs = TRUE, no.. = FALSE)
+    root_folder <- ""
+    if (grepl("NAMESPACE", rprojroot::is_r_package$find_file("NAMESPACE")))
+        root_folder <- system.file("extdata", package = "rNodal")
+    else
+        root_folder <- rprojroot::find_rstudio_root_file()
+    stopif(nchar(root_folder) == 0)
+    # stopifnot(length(user_root_folder))
+    list.files(path = root_folder, pattern = "*.h5$|*.hdf5$",
+               all.files = FALSE, full.names = TRUE, recursive = FALSE,
+               ignore.case = FALSE, include.dirs = FALSE, no.. = FALSE)
 }
 
+
+#' File info for all HDF5 files
+#' @export
+fileInfoHdf5 <- function() {
+    file.info(listAllHdf5())
+}
+
+#' Bigger HDF5
+#' @export
+biggerHdf5 <- function() {
+    df <- fileInfoHdf5()
+    row.names(df[which(max(df$size) == df$size),])
+}
+
+
+
+
+#' Ensure that R expressions are false
+#'
+#' @param ...
+#' Any number of (logical) R expressions,
+#' which should evaluate to \code{TRUE}.
+#' @export
+#' @examples
+#' \dontrun{
+#' stopif(is.empty(c(2,1)), 4 < 3) # all FALSE
+#' stopif(is.empty(numeric(0)))
+#' }
+stopif <- function(...)
+    {
+        n <- length(ll <- list(...))
+        if (n == 0L)
+            return(invisible())
+        mc <- match.call()
+        for (i in 1L:n) {
+            if (!(is.logical(r <- ll[[i]]) && !anyNA(r) && all(!r))) {
+                ch <- deparse(mc[[i + 1]], width.cutoff = 60L)
+                if (length(ch) > 1L)
+                    ch <- paste(ch[1L], "....")
+                stop(sprintf(ngettext(length(r), "%s is TRUE", "%s are not all FALSE"), ch),
+                     call. = FALSE, domain = NA)
+            }
+        }
+        invisible()
+    }
 
 
 #' Take objects and create a list using their names
@@ -30,5 +90,3 @@ named.list <- function(...) {
     # nl <- setNames( list(...) , as.character( match.call()[-1]) )
     nl
 }
-
-
