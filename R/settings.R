0868135e61ac7a1a521dbf542b08a2b291dc5d37
f0nzie zbook <zbook@oilgainsanalytics.com.com>
2017-06-03 16:10:16 -0500

    clean up settings.R

--- R/settings.R
+++ R/settings.R
@@ -2,22 +2,22 @@
 project.env <- new.env(parent = emptyenv()) # create new environment for project
 
 setProjectEnvironment <- function() {
-    data.file = "default"
+    data.file = "default"            # name for the HDF5 file
 
-    project.env[["project.root"]] <- proj_root <- system.file("..", package = "rNodal")
+    project.env[["project.root"]]    <- proj_root <- system.file("..", package = "rNodal")
     project.env[["project.extdata"]] <- proj_extdata <- paste(proj_root, "inst","extdata", sep = "/")
-    project.env[["project.data"]] <- proj_data <- paste(proj_root, "data", sep = "/")
+    project.env[["project.data"]]    <- proj_data <- paste(proj_root, "data", sep = "/")
 
     data.folder <- ReadFromProjectEnv("project.data")
     data.folder <- proj_extdata
 
     # file extensions for data file
-    datafile.h5 <- file.path(data.folder, paste(data.file, "h5", sep = "."))
+    datafile.h5   <- file.path(data.folder, paste(data.file, "h5", sep = "."))
     datafile.hdf5 <- file.path(data.folder, paste(data.file, "hdf5", sep = "."))
     datafile.rda  <- file.path(data.folder, paste(data.file, "rda", sep = "."))
 
-    project.env[["data.folder"]] <- data.folder
-    project.env[["data.file"]]   <- data.file
+    project.env[["data.folder"]]     <- data.folder
+    project.env[["data.file"]]       <- data.file
     project.env[["data.file.hdf5"]]  <- datafile.hdf5   # HDF5 file
     project.env[["data.file.rda"]]   <- datafile.rda     # RDA file
 }
