fcbb8b0a872ff168bfc98ca6b9a5de05b00701bc
f0nzie zbook <zbook@oilgainsanalytics.com.com>
2017-06-03 15:59:17 -0500

    small test for hdf5

---
+++ tests/testthat/test_paths.R
@@ -0,0 +1,6 @@
+library(testthat)
+#
+# pkg_root_folder <- system.file("..", package = "rNodal")
+# print(pkg_root_folder)
+# user_root_folder <- rprojroot::find_rstudio_root_file()
+# print(user_root_folder)
