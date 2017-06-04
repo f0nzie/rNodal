8fec87baa986ec7312d3dbb9501024097aed8761
f0nzie zbook <zbook@oilgainsanalytics.com.com>
2017-06-04 10:41:42 -0500

    add try catch

--- R/settings.R
+++ R/settings.R
@@ -2,24 +2,70 @@
 project.env <- new.env(parent = emptyenv()) # create new environment for project
 
 setProjectEnvironment <- function() {
-    data.file = "default"            # name for the HDF5 file
+    data_file = "default"            # name for the HDF5 file
 
-    project.env[["project.root"]]    <- proj_root <- system.file("..", package = "rNodal")
-    project.env[["project.extdata"]] <- proj_extdata <- paste(proj_root, "inst","extdata", sep = "/")
-    project.env[["project.data"]]    <- proj_data <- paste(proj_root, "data", sep = "/")
+    project.env[["pkg.root"]]    <- pkg_root <- system.file("..", package = "rNodal")
+    project.env[["pkg.extdata"]] <- pkg_extdata <- paste(pkg_root, "inst","extdata", sep = "/")
+    project.env[["pkg.data"]]    <- pkg_data <- paste(pkg_root, "data", sep = "/")
 
-    data.folder <- proj_extdata
+    data_folder <- pkg_extdata
 
-    # file extensions for data file
-    datafile.h5   <- file.path(data.folder, paste(data.file, "h5", sep = "."))
-    datafile.hdf5 <- file.path(data.folder, paste(data.file, "hdf5", sep = "."))
-    datafile.rda  <- file.path(data.folder, paste(data.file, "rda", sep = "."))
+    # full file path and extensions for data file
+    datafile.h5   <- file.path(data_folder, paste(data_file, "h5", sep = "."))
+    datafile.hdf5 <- file.path(data_folder, paste(data_file, "hdf5", sep = "."))
+    datafile.rda  <- file.path(data_folder, paste(data_file, "rda", sep = "."))
 
-    project.env[["data.folder"]]     <- data.folder
-    project.env[["data.file"]]       <- data.file
+    project.env[["data.folder"]]     <- data_folder
+    project.env[["data.file"]]       <- data_file
     project.env[["data.file.hdf5"]]  <- datafile.hdf5    # HDF5 file
     project.env[["data.file.rda"]]   <- datafile.rda     # RDA file
+
+    current_dir <- getwd()
+    print(current_dir)
+
+    # rstudio <- rstudioapi::getActiveDocumentContext()
+
+    # crit_r_pkg    <- rprojroot::is_r_package
+    # cat(isRpackage(), "\n")
+    # crit_r_studio <- rprojroot::is_rstudio_project
+
+    # this causing error during build
+    # if (isSavedSession()) {
+    #     cat("saved session")
+    #     load(getSessionFilename())
+    #     project.env[["data.file.hdf5"]] <- hdf5_file
+    # }
+
+    # out <- tryCatch(
+    #     {
+    #         message("this is the try part")
+    #         rprojroot::find_rstudio_root_file(path = "..")
+    #     },
+    #     error = function(e) {
+    #         message(e)
+    #         return(NA)
+    #     },
+    #     warning = function(w) {
+    #         message(w)
+    #         return(NULL)
+    #     },
+    #     finally = {
+    #         message("finally")
+    #     }
+    # )
+
+
 }
+
+
+isRpackage <- function() {
+    # crit_r_pkg    <- rprojroot::is_r_package
+    crit_r_pkg <- rprojroot::as.root_criterion("NAMESPACE")
+    # expected <-  "contains a file `DESCRIPTION` with contents matching `^Package: `"
+    expected <- "contains a file `NAMESPACE`"
+    ifelse(crit_r_pkg$desc == expected, TRUE, FALSE)
+}
+
 
 #' Save a variable to the project environment
 #' @param var variable to save in environment
