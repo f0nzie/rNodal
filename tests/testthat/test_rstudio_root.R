3590894c7ec8e578b7aaea16a1eacd8ecea5b20c
f0nzie zbook <zbook@oilgainsanalytics.com.com>
2017-06-04 10:42:33 -0500

    add tryCatch to a test

---
+++ tests/testthat/test_rstudio_root.R
@@ -0,0 +1,19 @@
+out <- tryCatch(
+    {
+        message("this is the try part")
+        rprojroot::find_rstudio_root_file(path = "..")
+    },
+    error = function(e) {
+        message(e)
+        return(NA)
+    },
+    warning = function(w) {
+        message(w)
+        return(NULL)
+    },
+    finally = {
+        message("finally")
+    }
+)
+
+out
