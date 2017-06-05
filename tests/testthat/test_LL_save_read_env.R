eb2ef84958d3d8ea8fd1a775d04cf6d6d4ebd254
f0nzie zbook <zbook@oilgainsanalytics.com.com>
2017-06-05 12:37:10 -0500

    change name

---
+++ tests/testthat/test_LL_save_read_env.R
@@ -0,0 +1,15 @@
+library(testthat)
+
+context("read save to environment")
+
+test_that("save value to environment", {
+    expect_equal(readFromProjectEnv("PRES.ATM"), 14.7)
+    saveToProjectEnv("PRES.ATM", 14.69)
+    expect_equal(readFromProjectEnv("PRES.ATM"), 14.69)
+})
+
+test_that("can read most important variables", {
+    expect_equal(readFromProjectEnv("TEMP.STD"), 60)
+    expect_equal(readFromProjectEnv("TEMP.RANKINE"), 460)
+
+})
