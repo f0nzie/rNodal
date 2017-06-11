a7a91314bc42716f94280aa67ff80d614d6e7fa4
f0nzie zbook <zbook@oilgainsanalytics.com.com>
2017-06-11 18:59:24 -0500

    fix name of folder when rNodal in different drive

--- tests/testthat/test_nodal_status.R
+++ tests/testthat/test_nodal_status.R
@@ -10,14 +10,16 @@ test_that("getwd() match folders when testing or checking", {
         expect_true(result == "C:/Users/msfz751/Documents/rNodal/tests/testthat" ||
             result == "I:/src/rNodal/tests/testthat")
     } else {
-        expect_equal(result, "C:/Users/msfz751/Documents/rNodal.Rcheck/tests/testthat")
+        expect_true(result == "C:/Users/msfz751/Documents/rNodal.Rcheck/tests/testthat" ||
+                        result == "I:/src/rNodal.Rcheck/tests/testthat")
     }
 })
 
 test_that("there is a HDF5 file under extdata", {
     result <- nodal_status()
     if (!is_checking_package()) {
-        expect_equal(result, "C:/Users/msfz751/Documents/rNodal/inst/extdata/default.hdf5")
+        expect_true(result == "C:/Users/msfz751/Documents/rNodal/inst/extdata/default.hdf5" ||
+                        result == "I:/src/rNodal/inst/extdata/default.hdf5")
     } else {
         # during devtools::check() the extdata files are saved under a Temp folder
         # "C:/Users/msfz751/AppData/Local/Temp/Rtmpwte0U6/RLIBS_36c05e7c1415/rNodal/inst/extdata/default.hdf5"
