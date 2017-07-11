a738f4517759554a4d90b42a2699ca49bfa8e261
Alfonso Reyes <alfonso.r.reyes@gmail.com>
2017-07-11 11:00:44 -0500

    add new script all_generics.R

---
+++ R/all_generics.R
@@ -0,0 +1,17 @@
+#' Set the VLP input for the well
+#'
+#' @param object a class object
+#' @param ... additional parameters
+#' @rdname set_vlp_input
+#' @export
+setGeneric("set_vlp_input",    function(object, ...) standardGeneric("set_vlp_input"))
+
+#' Set the VLP model definition
+#'
+#' @param object a class object
+#' @param ... additional parameters
+#' @rdname set_vlp_model
+#' @export
+setGeneric("set_vlp_model",    function(object, ...) standardGeneric("set_vlp_model"))
+
+setGeneric("run_vlp_wellhead", function(object, ...) standardGeneric("run_vlp_wellhead"))
