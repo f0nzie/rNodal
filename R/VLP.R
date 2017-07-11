3c53a45cc245ae4e4d96f871530dedba7b778c06
Alfonso Reyes <alfonso.r.reyes@gmail.com>
2017-07-10 22:22:49 -0500

    add function temp.fluid to calculate fluid temperature

--- R/VLP.R
+++ R/VLP.R
@@ -361,3 +361,25 @@ loadVLP <- function(model) {
     if (model == "dunsros.0")      return(dunsros.0)
     if (model == "fanbr.fanbr")    return(fanbr.fanbr)
 }
+
+
+
+# U <- 4
+# Cp <- (0.53 + 0.5 + 1) / 3
+temp.fluid <- function(well_table, theta, depth, bht, U, cp.avg, diam.ft, mass.rate) {
+    k <- U * pi * diam.ft / mass.rate / Cp
+    A <- 1 / k                   # relaxation distance by Ramey. Shoham, pg 297
+    Ti <- bht
+
+    for (i in nrow(well_table):1) {
+        L <- depth - well_table[i, "depth"]
+        Tei <- well_table[i, "temp"]
+        Ti <- (Tei - ge * L * sin(theta)) +
+            (Ti - Tei) * exp(-L/A) +
+            ge * A * sin(theta) * (1 - exp(-L/A))
+
+        # cat(sprintf("%3d %10.0f %10.2f \n", i, L, Ti))
+        well_table[i, "L"]  <- L
+        well_table[i, "Ti"] <- Ti
+    }
+}
