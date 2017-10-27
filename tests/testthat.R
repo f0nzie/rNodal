d53570582eb120a892ad2ea56895aad5ff1f883d
fonzie zbook <alfonso.r.reyes@gmail.com>
2017-10-27 14:21:20 -0500

    pkgdown build

--- tests/testthat.R
+++
@@ -1,6 +0,0 @@
-library(testthat)
-library(rNodal)
-test_check("rNodal")
-
-# report <- test_check("rNodal", reporter = "summary")
-# report <<- as.data.frame(report)
--- tests/testthat/test_LL_package_info.R
+++
@@ -1,9 +0,0 @@
-library(testthat)
-
-context("test if package installed")
-
-result <- R.utils::isPackageInstalled("rNodal")
-expect_true(result)
-
-result <- R.utils::isPackageLoaded("rNodal")
-expect_true(result)
