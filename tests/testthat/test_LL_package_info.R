20a075f12099d97fe4631d8c9e0b69315b6d4c39
f0nzie zbook G4 <alfonso.reyes@oilgainsanalytics.com>
2019-02-02 20:00:36 -0600

    add test_that

--- tests/testthat/test_LL_package_info.R
+++ tests/testthat/test_LL_package_info.R
@@ -1,9 +1,15 @@
 library(testthat)
+# test functions in package R.utils
 
 context("test if package installed")
 
-result <- R.utils::isPackageInstalled("rNodal")
-expect_true(result)
+test_that("rNodal is installed", {
+    result <- R.utils::isPackageInstalled("rNodal")
+    expect_true(result)
+})
 
-result <- R.utils::isPackageLoaded("rNodal")
-expect_true(result)
+test_that("rNodal is loaded", {
+    result <- R.utils::isPackageLoaded("rNodal")
+    expect_true(result)
+})
+
