edfddaf2a37b4420fb976f90a50318a4ef317a52
f0nzie zbook G4 <alfonso.reyes@oilgainsanalytics.com>
2019-02-06 17:25:26 -0600

    remove hdf5 from environment store

--- R/settings.R
+++ R/settings.R
@@ -2,22 +2,19 @@
 project.env <- new.env(parent = emptyenv()) # create new environment for project
 
 setProjectEnvironment <- function() {
-    data_file = "default"            # name for the HDF5 file
 
-    project.env[["pkg.root"]]    <- pkg_root <- system.file("..", package = "rNodal")
-    project.env[["pkg.extdata"]] <- pkg_extdata <- paste(pkg_root, "inst","extdata", sep = "/")
+    project.env[["pkg.root"]]    <- pkg_root <- system.file("..",
+                                                            package = "rNodal")
+    project.env[["pkg.extdata"]] <- pkg_extdata <- paste(
+        pkg_root, "inst","extdata", sep = "/")
     project.env[["pkg.data"]]    <- pkg_data <- paste(pkg_root, "data", sep = "/")
 
     data_folder <- pkg_extdata
 
     # full file path and extensions for data file
-    datafile.h5   <- file.path(data_folder, paste(data_file, "h5", sep = "."))
-    datafile.hdf5 <- file.path(data_folder, paste(data_file, "hdf5", sep = "."))
     datafile.rda  <- file.path(data_folder, paste(data_file, "rda", sep = "."))
 
     project.env[["data.folder"]]     <- data_folder
-    project.env[["data.file"]]       <- data_file
-    project.env[["data.file.hdf5"]]  <- datafile.hdf5    # HDF5 file
     project.env[["data.file.rda"]]   <- datafile.rda     # RDA file
 
 
@@ -26,7 +23,6 @@ setProjectEnvironment <- function() {
         # it is a user project folder. Not in building package mode
         if (is_saved_session()) {
             name_of_the_file <- load(getSessionFilename())
-            project.env[["data.file.hdf5"]] <- get(name_of_the_file)
         } else {
             # cat("Save session not found\n")
         }
