faa02a90eb665fe9126f07a8f71fc9a175ede10e
f0nzie zbook G4 <alfonso.reyes@oilgainsanalytics.com>
2019-02-06 18:12:37 -0600

    enable couple of tests

--- tests/testthat/test_LL_paths.R
+++ tests/testthat/test_LL_paths.R
@@ -10,23 +10,33 @@ test_that("testthat is the last folder in getwd()",
 test_that("relative path is a dot",
           expect_equal(R.utils::getRelativePath(getwd()), ".") )
 
-# test_that("parent folder of getwd() is tests", {
-#     res <- unlist(strsplit(R.utils::getParent(getwd()), split = "/"))
-#     .res <- res[length(res)]
-#     expect_equal(.res, "tests")
-# })
+test_that("parent folder of getwd() is tests", {
+    # split the current path
+    res <- unlist(strsplit(R.utils::getParent(getwd()), split = "/"))
+    .res <- res[length(res)] # get the last part of the path
+    cat("\n")
+    print(res)
+    print(.res)
+    expect_equal(.res, "tests")
+})
 
-# test_that("system.file(pkg) creates Temp dir at check time", {
-#     res <- unlist(strsplit(system.file(package = "rNodal"), split = "/"))
-#     .res <- paste(res[length(res)-1], res[length(res)], sep = "/")
-#     if (is_checking_package()) {
-#         # happens during check
-#         expect_true("Temp" %in% res)
-#         expect_true("rNodal" %in% res)
-#     } else {
-#         # happens when only testing
-#         expect_equal(.res, "rNodal/inst")
-#     }
-# })
+test_that("system.file(pkg) creates a Temp dir at check time", {
+    res <- unlist(strsplit(system.file(package = "rNodal"), split = "/"))
+    # join previous and next string
+    .res <- paste(res[length(res)-1], res[length(res)], sep = "/")
+    cat("\n")
+    print(res)
+    print(.res)
+    if (is_checking_package()) {
+        # happens during check
+        cat("in package ...")
+        expect_true("Temp" %in% res)
+        expect_true("rNodal" %in% res)
+    } else {
+        # happens when only testing
+        cat("\t testing now ...")
+        expect_equal(.res, "rNodal/inst")
+    }
+})
 
 
