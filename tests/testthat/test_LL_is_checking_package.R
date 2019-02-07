807bd9809f2a43f6623b6e2bc0343081ce503a1d
f0nzie zbook G4 <alfonso.reyes@oilgainsanalytics.com>
2019-02-06 18:31:12 -0600

    enable tests

--- tests/testthat/test_LL_is_checking_package.R
+++ tests/testthat/test_LL_is_checking_package.R
@@ -3,28 +3,40 @@ library(testthat)
 context("test is_checking_package function")
 
 prj_dir <- R.utils::getParent(R.utils::getParent(getwd()))
-# from check:
+# from check package mode:
 #      "C:/Users/msfz751/Documents/rNodal.Rcheck"
-# from test:
+# from testing mode:
 #      "C:/Users/msfz751/Documents/rNodal"
 
-# test_that("the folder for rNodal changes during devtools::check", {
-#     if (is_checking_package())
-#         expect_true("rNodal.Rcheck" %in% unlist(strsplit(getwd(), split = "/")))
-# })
+test_that("the folder for rNodal changes during devtools::check", {
+    if (is_checking_package())
+        expect_true("rNodal.Rcheck" %in% unlist(strsplit(getwd(), split = "/")))
+    else
+        expect_false(is_checking_package())
+})
 
-# test_that(".Rcheck is part of the getwd() path when devtools::check", {
-#     if (is_checking_package())
-#         expect_true(grepl(".Rcheck", prj_dir))
-# })
-#
-#
-# test_that("getwd() contains these member in its path", {
-#     result <- unlist(strsplit(getwd(), split = "/"))
-#     if (is_checking_package()) {
-#         # expect_true(is_checking_package())
-#         expect_true("testthat" %in% result)
-#         expect_true("tests" %in% result)
-#         expect_true("rNodal.Rcheck" %in% result)
-#     }
-# })
+
+test_that(".Rcheck is part of the getwd() path when devtools::check", {
+    if (is_checking_package())
+        expect_true(grepl(".Rcheck", prj_dir))
+    else
+        expect_false(grepl(".Rcheck", prj_dir))
+})
+
+
+test_that("getwd() contains these member in its path", {
+    result <- unlist(strsplit(getwd(), split = "/"))
+    print(result)
+    if (is_checking_package()) {
+        expect_true("testthat" %in% result)
+        expect_true("tests" %in% result)
+        expect_true("rNodal.Rcheck" %in% result)
+    }
+    else {
+        # not building package but testing
+        expect_true("testthat" %in% result)
+        expect_true("tests" %in% result)
+        expect_false("rNodal.Rcheck" %in% result)
+    }
+
+})
